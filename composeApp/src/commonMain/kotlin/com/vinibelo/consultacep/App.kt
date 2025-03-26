ef48334b9d3c882699f2a5b169acb514a0f8b6b0
ViniBelo <vinialvesbelo@outlook.com>
2025-03-26 18:04:43 -0300

    Added material3

--- composeApp/src/commonMain/kotlin/com/vinibelo/consultacep/App.kt
+++ composeApp/src/commonMain/kotlin/com/vinibelo/consultacep/App.kt
@@ -1,20 +1,8 @@
 package com.vinibelo.consultacep
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.runtime.Composable
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import consultacep.composeapp.generated.resources.Res
-import consultacep.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
