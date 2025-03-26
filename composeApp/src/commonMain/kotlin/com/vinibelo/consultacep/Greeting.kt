f11564d106c4fec7ead97765fe06142c8eb25b3c
ViniBelo <vinialvesbelo@outlook.com>
2025-03-26 17:48:45 -0300

    Removed unnecessary code

--- composeApp/src/commonMain/kotlin/com/vinibelo/consultacep/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.vinibelo.consultacep
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/com/vinibelo/consultacep/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.vinibelo.consultacep
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
